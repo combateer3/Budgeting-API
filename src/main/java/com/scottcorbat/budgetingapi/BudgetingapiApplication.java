a602ba5014b0d82a83c104a9ff449815bdcea9f1
combateer3 <scottcorbat@yahoo.com>
2022-03-16 19:27:55 -0400

    initial commit

---
+++ src/main/java/com/scottcorbat/budgetingapi/BudgetingapiApplication.java
@@ -0,0 +1,13 @@
+package com.scottcorbat.budgetingapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetingapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetingapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/scottcorbat/budgetingapi/BudgetingapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scottcorbat.budgetingapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetingapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
